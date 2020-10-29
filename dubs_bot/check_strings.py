f1062b2b7d35cdd49ca6315eaac27daf7aaed189
Nick80835 <nick80835@gmail.com>
2020-10-29 13:29:07 -0400

    reformat checks, fix some mistakes

--- dubs_bot/check_strings.py
+++ dubs_bot/check_strings.py
@@ -1,29 +1,125 @@
 dubs_dict = {
-    2: ["CHECK 'EM", "nice dubs!!!", "2 same number", "top dubs!!", "in dubs we trust!!", "dubs", "nice dubs fren", "*ckecks the dubs!*", "OH GOD IM CHEEEECKING!!!", "dubs!!"],
-    3: ["CHECK 'EM!!!", "sweet trips", "TRIPS!!!", "oh baby a trips!!", "top trips!!!", "*ckecks the trips!*", "these trips shant be left unchecked!", "OH GOD IM CHEEEECKING!!!", "trips!!!"],
-    4: ["sexy quads!!", "HOLY COW QUADS!!!", "SWEET QUADS FREN!!!", "*ckecks the quads!*", "these quads shant be left unchecked!", "very nice tripsies", "Trips confirm, obey the law and always check those digits", "OH GOD IM CHEEEECKING!!!", "quads!!!!"],
-    5: ["CHECKIN' QUINTS!!!", "QUINTS WOW!!!", "Sweet quints fren!!", "Quints confirm, obey the law and always check those digits", "OH GOD IM CHEEEECKING!!!", "quints!!!!!"],
-    6: ["SEX!!", "CHECKIN' SEXTUPLES!!!!!!", "SIX SAME NUMBER WOW!!!", "OH GOD IM CHEEEECKING!!!"],
-    7: ["CHECKIN' SEPTUPLES!!!", "7 SAME NUMBER HOLY MOLY!!!", "NICE SEPTS FREN!!!"],
-    8: ["HOLY SHIT 8 SAME NUMBER", "OCTUPLES CHECKED!!!!"],
-    9: ["HOW"],
-    10: ["WHAT THE FUCK!?!?!"]
+    2: [
+        "CHECK 'EM",
+        "nice dubs!!!",
+        "2 same number",
+        "top dubs!!",
+        "in dubs we trust!!",
+        "dubs",
+        "nice dubs fren",
+        "*ckecks the dubs!*",
+        "OH GOD IM CHEEEECKING!!!",
+        "dubs!!",
+        "you got a nice pair there",
+        "sweet pair"
+    ],
+    3: [
+        "CHECK 'EM!!!",
+        "sweet trips",
+        "TRIPS!!!",
+        "oh baby a trips!!",
+        "top trips!!!",
+        "*ckecks the trips!*",
+        "these trips shant be left unchecked!",
+        "OH GOD IM CHEEEECKING!!!",
+        "trips!!!",
+        "triplets",
+        "very nice tripsies",
+        "Trips confirm, obey the law and always check those digits"
+    ],
+    4: [
+        "sexy quads!!",
+        "HOLY COW QUADS!!!",
+        "SWEET QUADS FREN!!!",
+        "*ckecks the quads!*",
+        "these quads shant be left unchecked!",
+        "OH GOD IM CHEEEECKING!!!",
+        "quads!!!!"
+    ],
+    5: [
+        "CHECKIN' QUINTS!!!",
+        "QUINTS WOW!!!",
+        "Sweet quints fren!!",
+        "Quints confirm, obey the law and always check those digits",
+        "OH GOD IM CHEEEECKING!!!",
+        "quints!!!!!"
+    ],
+    6: [
+        "SEX!!",
+        "CHECKIN' SEXTUPLES!!!!!!",
+        "SIX SAME NUMBER WOW!!!",
+        "OH GOD IM CHEEEECKING!!!"
+    ],
+    7: [
+        "CHECKIN' SEPTUPLES!!!",
+        "7 SAME NUMBER HOLY MOLY!!!",
+        "NICE SEPTS FREN!!!"
+    ],
+    8: [
+        "HOLY SHIT 8 SAME NUMBER",
+        "OCTUPLES CHECKED!!!!"
+    ],
+    9: [
+        "HOW"
+    ],
+    10: [
+        "WHAT THE FUCK!?!?!"
+    ]
 }
 
 special_dubs_dict = {
-    666: ["absolutely demonic", "I dont like those numbers"],
-    777: ["holy trips!!", "blessed trips"],
-    555: ["angelic trips!!"],
-    333: ["three threes"],
-    22: ["two twos"],
-    4444: ["4 by 4", "four fours"],
-    7777777: ["7777777 GET!!!", "blessed be the se7en"]
+    666: [
+        "absolutely demonic",
+        "I dont like those numbers"
+    ],
+    777: [
+        "holy trips!!",
+        "blessed trips"
+    ],
+    555: [
+        "angelic trips!!"
+    ],
+    333: [
+        "three threes"
+    ],
+    22: [
+        "two twos"
+    ],
+    4444: [
+        "4 by 4",
+        "four fours"
+    ],
+    7777777: [
+        "7777777 GET!!!",
+        "blessed be the se7en"
+    ]
 }
 
 not_dubs_but_funny_dict = {
-    6969: ["double sex", "double sex number", "haha funi", "sexy sex"],
-    69: ["haha funi numbr", "69 funny", "LOLE", "funny sex number"],
-    420: ["WEED GET", "weed", "funny weed number"],
-    455: ["ass", "ASS"],
-    545: ["s4s", "S4S", "[s4s]"]
+    6969: [
+        "double sex",
+        "double sex number",
+        "haha funi",
+        "sexy sex"
+    ],
+    69: [
+        "haha funi numbr",
+        "69 funny",
+        "LOLE",
+        "funny sex number"
+    ],
+    420: [
+        "WEED GET",
+        "weed",
+        "funny weed number"
+    ],
+    455: [
+        "ass",
+        "ASS"
+    ],
+    545: [
+        "s4s",
+        "S4S",
+        "[s4s]"
+    ]
 }
